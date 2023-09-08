70956b8d1edce333c47ed04099d4b51435ef608a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-08 10:07:12 -0300

    prueba

--- TP-DAI/Parte-RN/src/Screens/Home.js
+++ TP-DAI/Parte-RN/src/Screens/Home.js
@@ -1,18 +1,21 @@
 import { useState} from 'react'
-import {View, StyleSheet ,Text} from 'react-native'
+import {View, StyleSheet ,Text, Button} from 'react-native'
 import axios from 'axios'
 
 export default function Home({route}) {
-    const [nombreCompleto, setNombreCompleto] = useState('')
-    const { Usuario, Contraseña } = route.params
+    const [usuario, setUsuario] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
     axios.post('http://localhost:5000/usuario', {
         Nombre: Usuario,
         Contraseña: Contraseña
     })
-        .then()
+        .then(function(response) {
+            setUsuario(response.data)
+        })
+        .finally(() => setIsLoading(false))
     return (
         <View>
-            <Text>Bienvenido {nombreCompleto}</Text>
+            {isLoading ? null : usuario.NombreCompleto === null ? <Text>Bienvenido <Button>Completar perfil</Button></Text> : <Text>Bienvenido {usuario.NombreCompleto}</Text>}
         </View>
     )
 }
