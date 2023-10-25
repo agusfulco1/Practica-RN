1db9a58c5d3680925df87743f2a00be8d3a7ff4e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-25 09:05:26 -0300

    firebase

--- TP-DAI/Parte-RN/App.js
+++ TP-DAI/Parte-RN/App.js
@@ -1,5 +1,23 @@
 import StackScreen from "./src/Navigation/Stack"
 import { NavigationContainer } from '@react-navigation/native';
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArdTNAB2okxowH24e0oD5466X7yb9A4cw",
+  authDomain: "practica-rn.firebaseapp.com",
+  projectId: "practica-rn",
+  storageBucket: "practica-rn.appspot.com",
+  messagingSenderId: "84498110057",
+  appId: "1:84498110057:web:e24b62519119eba93274f8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
 export default function App() {
   return (
     <NavigationContainer>
